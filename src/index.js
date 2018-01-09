819268febfb21f1a4995c07b1f079cf9404db468
Erik Hals <erikhals@gmail.com>
2018-01-09 08:22:58 +0100

    Add firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as firebase from 'firebase'
+
+import App from './App'
+
+
+const config = {
+    apiKey: "AIzaSyDA3OKvkpat-9LFYSG0GnfY2xrfpLmz4aE",
+    authDomain: "micetro-poeng.firebaseapp.com",
+    databaseURL: "https://micetro-poeng.firebaseio.com",
+    projectId: "micetro-poeng",
+    storageBucket: "micetro-poeng.appspot.com",
+    messagingSenderId: "38039570025"
+  }
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <App />,
