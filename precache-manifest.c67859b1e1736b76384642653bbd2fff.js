b1f7db0d6f4320c94168a785e5387ec41c87f7a0
Erik Hals <erikhals@gmail.com>
2020-08-21 23:10:22 +0200

    Updates

---
+++ precache-manifest.c67859b1e1736b76384642653bbd2fff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f3108cef927fbf85511",
+    "url": "/micetro-poeng-react/static/js/main.5f3108ce.chunk.js"
+  },
+  {
+    "revision": "2ce59bfc9da529ba00b6",
+    "url": "/micetro-poeng-react/static/js/1.2ce59bfc.chunk.js"
+  },
+  {
+    "revision": "d63067fa4e0aa85a1167",
+    "url": "/micetro-poeng-react/static/js/runtime~main.d63067fa.js"
+  },
+  {
+    "revision": "d9a6b7f05edace517fbfd8ac96429493",
+    "url": "/micetro-poeng-react/index.html"
+  }
+];
